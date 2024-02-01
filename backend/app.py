55f945d4e9f34c8d35eb5ce31f1414a128d7fd61
MaggieLOL <tanhanze@qq.com>
2024-02-01 17:44:46 +0800

    move

--- backend/app.py
+++ backend/app.py
@@ -5,5 +5,5 @@ app=Flask(__name__, static_folder='./static', template_folder='./templates')
 @app.errorhandler(500)
 def error(e):
     return {"code":500,"text":"Internal server error"}, 500
-app.add_url_rule("/v1/create_user", view_func=create_user, methods=['POST'])
-app.add_url_rule("/v1/get_user_details", view_func=get_user_details, methods=['GET'])
+app.add_url_rule("/api/v1/create_user", view_func=create_user, methods=['POST'])
+app.add_url_rule("/api/v1/get_user_details", view_func=get_user_details, methods=['GET'])
