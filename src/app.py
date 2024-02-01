65d37a1596b5cc677cd33497d094d840f939ffb3
MaggieLOL <tanhanze@qq.com>
2024-02-01 14:47:35 +0800

    Update backend development script

--- src/app.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask, render_template
-
-app: Flask = Flask(__name__)
-
-app.run(debug = True)
--- src/backend/app.py
+++ src/backend/app.py
@@ -7,4 +7,3 @@ def error(e):
     return {"code":500,"text":"Internal server error"}, 500
 app.add_url_rule("/v1/create_user", view_func=create_user, methods=['POST'])
 app.add_url_rule("/v1/get_user_details", view_func=get_user_details, methods=['GET'])
-app.run(debug=True)
